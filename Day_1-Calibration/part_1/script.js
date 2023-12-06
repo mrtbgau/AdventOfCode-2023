3d72fff3b312e3780065676966986d4c997e402c
mrtbgau <mrt.bgau@gmail.com>
2023-12-06 23:02:10 +0100

    :rocket: End of part_1

--- Day_1-Calibration/part_1/script.js
+++ Day_1-Calibration/part_1/script.js
@@ -1,4 +1,32 @@
 import fs from "node:fs";
 
-const content = fs.readFileSync("values.txt");
-console.log(content);
+const content = fs.readFileSync("values.txt", { encoding: "utf8" });
+
+const lines = content.split("\n");
+
+let sum = 0;
+
+lines.forEach((line) => {
+  let numbersInLine = [];
+
+  for (const char of line) {
+    if (!isNaN(char)) {
+      numbersInLine.push(char.toString());
+    }
+  }
+
+  const cleanedNumbersInLine = numbersInLine.filter(
+    (value) => !isNaN(parseInt(value))
+  );
+
+  if (cleanedNumbersInLine.length === 1) {
+    sum += parseInt(cleanedNumbersInLine[0] + cleanedNumbersInLine[0]);
+  } else if (cleanedNumbersInLine.length > 1) {
+    sum += parseInt(
+      cleanedNumbersInLine[0] +
+        cleanedNumbersInLine[cleanedNumbersInLine.length - 1]
+    );
+  }
+});
+
+console.log("Sum:", sum);
